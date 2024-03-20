001c6e0f8910cc870b753f4fb61f24c315dea3b5
Bui Minh Tinh <buiminhtinh01@gmail.com>
2024-03-20 09:27:52 +0700

    đẹp

--- js/main.js
+++
@@ -1,5 +0,0 @@
-function activateTab(element) {
-        var allTabs = document.querySelectorAll('.nav-link');
-        allTabs.forEach(tab => tab.classList.remove('active'));
-        element.classList.add('active');
-}
