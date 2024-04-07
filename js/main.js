303d1b7eddce78a2a25a753a78725fa0e5a7a881
Haotlppc07447 <137756157+Haotlppc07447@users.noreply.github.com>
2024-04-07 19:08:16 +0700

    Create page About-us

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ function activateTab(element) {
         allTabs.forEach(tab => tab.classList.remove('active'));
         element.classList.add('active');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+        var navbarToggler = document.querySelector(".navbar-toggler");
+        var navbarCollapse = document.querySelector(".navbar-collapse");
+        var overlay = document.getElementById("overlay");
+
+        navbarToggler.addEventListener("click", function () {
+            navbarCollapse.classList.toggle("show");
+            overlay.classList.toggle("active");
+        });
+        document.addEventListener("click", function (event) {
+            if (!navbarToggler.contains(event.target) && !navbarCollapse.contains(event.target)) {
+                navbarCollapse.classList.remove("show");
+                overlay.classList.remove("active");
+            }
+        });
+        overlay.addEventListener("click", function () {
+            navbarCollapse.classList.remove("show");
+            overlay.classList.remove("active");
+        });
+    });
